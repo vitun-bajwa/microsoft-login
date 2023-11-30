bc2110717a08dc69c7bc338998566383530639f1
vitun bajwa <vitunbajwawins@gmail.com>
2023-11-30 10:11:25 +0530

    login with microsoft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'azured-angular-demo';
+  constructor(private msalService: MsalService) {
+  }
+
+  ngOnInit(): void {
+    // this.msalService.instance.handleRedirectPromise().then(
+    //   (res:any) => {
+    //     if(res != null && res.account !=null){
+    //   this.msalService.instance.setActiveAccount(res.account)
+    //     }
+    //   }
+    // )
+  }
+  isLoggedin(): boolean {
+    return this.msalService.instance.getActiveAccount() != null;
+  }
+
+  login() {
+    // this.msalService.loginRedirect();
+    this.msalService.loginPopup().subscribe((response: AuthenticationResult) => {
+    this.msalService.instance.setActiveAccount(response.account)
+    });
+  }
+
+  logout() {
+    this.msalService.logoutPopup();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MsalService, MSAL_INSTANCE, MsalModule } from "@azure/msal-angular";
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+export function MSALInstanceFactory(): IPublicClientApplication{
+  return new PublicClientApplication({
+    auth: {
+      clientId: '52b54be9-66bb-4e92-b674-93fdd15a5661',
+      redirectUri: 'http://localhost:4200'
+    }
+  })
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MsalModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
